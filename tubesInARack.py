70705ac26ce9c03b50d22d4e101386c42737c9bf
Sourcery AI <>
2022-10-07 05:21:00 +0000

    'Refactored by Sourcery'

--- tubesInARack.py
+++ tubesInARack.py
@@ -3,12 +3,12 @@
 import argparse
 import numpy as np
 
-def	main():
+def main():
 	parser = argparse.ArgumentParser(description="**This program performs move or remove operations and returns"+
 			"the final positions of tubes in the rack**")
-	
+
 	requiredArgs = parser.add_argument_group('Required arguments')
-	
+
 	requiredArgs.add_argument('--rack_size', nargs='+', type=int, required=True, help="Size of the square rack:\n" +
 			"please provide the capacity of the rack in a single row or column).", dest='rackSize')
 	requiredArgs.add_argument('--tube_positions', metavar='FILE', type=argparse.FileType('r'), required=True, help="Name of\n"+
@@ -22,11 +22,11 @@ def	main():
 			"tab delimited text file containing (x, y) coordinates of the tubes to be removed, one in\n" +
 			"each row." , dest='removeList')
 	args = parser.parse_args()
-	
+
 	N = args.rackSize
-	
+
 	initPos = args.initialPositions.read()
-	
+
 	# Following lines consist of series of text processing operations to convert the input coordinates into a binary matrix
 	temp1 = initPos.replace("(","").replace(")","")
 	temp2 = temp1.split('\n')
@@ -35,14 +35,14 @@ def	main():
 	temp5 = [int(i) for i in temp4]
 	list1 = ([(temp5[i],temp5[i+1]) for i in range(0,len(temp5),2)])
 	array1 = np.array(list1)
-	
+
 	# PAM: presence-absence matrix
-	
+
 	PAM = np.zeros((N[0],N[0]), dtype=int)	# initializing a zero matrix of size 'N X N'
 	PAM[array1[:,0], array1[:,1]] = 1	# binarizing the matrix. 1: Presence of tube, 0: Absence of tube
-	
+
 	moves_list = args.moves.read()
-	
+
 	# Following lines consist of series of text processing operations to convert the input move operations to indices of PAM
 	temp1 = moves_list.replace("(","").replace(")","")
 	temp2 = temp1.split('\n')
@@ -52,9 +52,9 @@ def	main():
 	temp6 = [int(i) for i in temp5]
 	list2 = ([(temp6[i],temp6[i+1]) for i in range(0,len(temp6),2)])
 	array2 = np.array(list2)
-	
+
 	remove_list = args.removeList.read()
-	
+
 	# Following lines consist of series of text processing operations to convert the input remove operations to indices of PAM
 	temp1 = remove_list.replace("(","").replace(")","")
 	temp2 = temp1.split('\n')
@@ -63,43 +63,41 @@ def	main():
 	temp5 = [int(i) for i in temp4]
 	list3 = ([(temp5[i],temp5[i+1]) for i in range(0,len(temp5),2)])
 	array3 = np.array(list3)
-	
+
 	# Move operations
 	j=0
-	for i in range(int(len(array2)/2)):
+	for _ in range(len(array2) // 2):
 		if PAM[array2[j+1][0]][array2[j+1][1]]==0:
 			PAM[array2[j][0]][array2[j][1]]=0
 			PAM[array2[j+1][0]][array2[j+1][1]]=1
 		j=j+2
-	
+
 	# Remove operations
 	j=0
-	for i in range(len(array3)):
+	for _ in range(len(array3)):
 		if PAM[array3[j][0]][array3[j][1]]==1:
 			PAM[array3[j][0]][array3[j][1]]=0
 		j=j+1
-	
+
 	PAMnp = np.array(PAM)
 	PAMnp_nonzero = PAMnp.nonzero()	# Obtaining to coordinates (indices) of the final positions of tubes in the rack
-	
+
 	# The following lines save the output to a text file
-	
+
 	k=0
-	myfile = open('finaltubepositions.txt','w')
-	for j in range(len(PAMnp_nonzero[0])):
-		myfile.write("({0}, {1})\n" .format(PAMnp_nonzero[0][k],PAMnp_nonzero[1][k]))
-	k=k+1
-	
-	array3np = np.array(array3)
-	k=0
-	for j in range(len(array3np)):
-		myfile.write("({0}, {1})\tremoved\n" .format(array3np[k][0],array3np[k][1]))
-		k=k+1
-	myfile.close()
-	outfile = open('finaltubepositions.txt','r')
-	print("\n")
-	print(outfile.read())
-	outfile.close()
+	with open('finaltubepositions.txt','w') as myfile:
+		for _ in range(len(PAMnp_nonzero[0])):
+			myfile.write("({0}, {1})\n" .format(PAMnp_nonzero[0][k],PAMnp_nonzero[1][k]))
+		k += 1
+
+		array3np = np.array(array3)
+		k=0
+		for _ in range(len(array3np)):
+			myfile.write("({0}, {1})\tremoved\n" .format(array3np[k][0],array3np[k][1]))
+			k=k+1
+	with open('finaltubepositions.txt','r') as outfile:
+		print("\n")
+		print(outfile.read())
 	print("\nThe output shown above has been saved in 'finaltubepositions.txt'\n")
 	
 if __name__ == '__main__':
